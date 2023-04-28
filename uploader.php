ac9120d4edda2e98fe21117f267072643d26273a
Игорь <124060634+Oliferenko@users.noreply.github.com>
2023-04-28 19:50:11 +0300

    Delete uploader.php

--- uploader.php
+++
@@ -1,5 +0,0 @@
-<?php
-$uploaddir = '../img/'; // <- это всего лишь строка
-$uploadfile = $uploaddir.$_FILES['файл пользователя']['имя'];
-move_uploaded_file($_FILES['файл пользователя']["tmp_name"], $uploadfile);
-$ uploadfileecho;
